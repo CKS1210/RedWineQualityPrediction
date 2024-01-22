17a37a2a8d2058bf031d07b6ca5e91b24c6af1fd
CKS1210 <kaisin1993@hotmail.com>
2024-01-22 10:16:44 +0800

    data validation added

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from src.RedWineQualityPrediction import logger
 from src.RedWineQualityPrediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.RedWineQualityPrediction.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 
 STAGE_NAME = "Data Ingestion Stage"
-
 try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data DataValidationalidation Stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=================x")
 except Exception as e:
